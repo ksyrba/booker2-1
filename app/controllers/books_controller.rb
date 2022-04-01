acb47eff6b89d0218108a9f8a8965af5d47e32dd
ksyrba <ksyrbaba0812@gmail.com>
2022-04-01 08:04:42 +0000

    [finish]7a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,9 +9,14 @@ class BooksController < ApplicationController
   end
 
   def index
-
     @book_new = Book.new
-    @books = Book.includes(:favorited_users).where(created_at: Time.current.all_week).sort {|a,b| b.favorited_users.size <=> a.favorited_users.size}
+    @books = Book.includes(:favorited_users).
+      where(created_at: Time.current.all_week).
+      sort {|a,b| 
+      b.favorited_users.size <=> 
+      a.favorited_users.size
+        
+      }
   end
 
   def create
